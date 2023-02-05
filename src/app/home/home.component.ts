88bf88f33e69c0f0528b5d226d69ecbea7014450
Rintugithub <rintuva000@gmail.com>
2023-02-05 20:47:43 +0530

    color change code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
- username:string="";
+ export class HomeComponent implements OnInit,AfterViewInit {
+  value!:string;
+ username:any;
+ data:string="hai";
+ colours:string[]=["red","green","blue","orange","yellow","lightgreen","lightblue"];
+ colourval:any;
+ @ViewChild("col")marker!:ElementRef<any>;
   constructor() { }
+  ngAfterViewInit(): void {
+
+  }
 
   ngOnInit(): void {
+    // console.log(this.value);
+
+
+
   }
+
+  getval(item:any){
+  console.log((item.target as HTMLInputElement).value);
+  this.username= (item.target as HTMLInputElement).value;
+}
+
+changebg(color:any){
+  console.log((color.target as HTMLInputElement).value);
+  this.colourval= (color.target as HTMLInputElement).value;
+  this.marker.nativeElement.style.backgroundColor = this.colourval;
+
+
+}
 
 }
