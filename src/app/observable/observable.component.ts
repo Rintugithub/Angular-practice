c7a1963a2f98712f3bedfbc181cb2b2ebfaf2f1c
Rintugithub <rintuva000@gmail.com>
2023-02-14 07:28:44 +0530

    code changes

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-observable',
@@ -10,6 +11,40 @@ export class ObservableComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let promise = new Promise((res,rej)=>{
+       setTimeout(() => {
+      // res("promise working");
+      rej("failed");
+      }, 1000);
+
+    });
+    const val=async()=>{
+      try{
+        let data = await promise;
+        console.log(data);
+      }catch(error){
+        console.log(error);
+
+
+      }
+
+
+    }
+    val();
+    // promise.then(result=>console.log(result));
+    const observable= new Observable(res=>{
+      setTimeout(() => {
+        res.next("observable working");
+        res.next("observable working1");
+        res.next("observable working2");
+        res.next("observable working3");
+
+
+      }, 1000);
+    });
+    observable.subscribe(res=> console.log(res));
+
   }
+
 
 }
