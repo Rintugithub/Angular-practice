93d40f6f17d63af29395cacb9386f49cc6ea54da
Rintugithub <rintuva000@gmail.com>
2023-02-15 12:21:51 +0530

    ng switch

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,12 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  selectedcountry:any;
+  countries:any[]=[
+    {code:"ind",country:"India"},
+    {code:"aus",country:"Australia"},
+    {code:"nz",country:"Newzeland"},
+    {code:"chi",country:"China"},
+    {code:"rus",country:"Russia"},
+
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
     console.log("product component");
 
+  }
+  choice(code:any){
+    this.selectedcountry = (code.target as HTMLInputElement).value;
   }
 
 }
